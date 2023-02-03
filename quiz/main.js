d94e614885599b72b844bdfc9468796402156604
aarena18 <ambrearena@gmail.com>
2023-02-03 13:38:51 +0100

    :sparkles: deploy new script feature

--- quiz/main.js
+++ quiz/main.js
@@ -1,10 +1,18 @@
 // Fonction pour calculer les bonnes réponses et donner le résultat à la fin
-const correctAnswer = document.querySelectorAll('#correct')
-let score = 0
+const correctAnswer = document.querySelectorAll("#correct");
+let score = 0;
 function result() {
-    score = 0
-    correctAnswer.forEach(answer => {
-        answer.checked ? score++ : score
-    })
-    document.getElementById('my-score').innerText = `Vous avez marqué ${score} points`
+  score = 0;
+  correctAnswer.forEach((answer) => {
+    answer.checked ? score++ : score;
+  });
+  document.getElementById(
+    "my-score"
+  ).innerText = `Vous avez marqué ${score} points.`;
+
+  if (score === 3) {
+    document.getElementById(
+      "my-score"
+    ).innerText += ` 🎉 Bravo, vous avez tout bon 🎉 !`;
+  }
 }
